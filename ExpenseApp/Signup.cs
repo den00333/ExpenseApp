804fb32fc1554062b391a0ab923364660b93231b
Macmac331 <bautistamacmac331@gmail.com>
2023-10-19 21:11:34 +0800

    added some validations

--- ExpenseApp/Signup.cs
+++ ExpenseApp/Signup.cs
@@ -183,7 +183,7 @@ namespace ExpenseApp
 
         private void btnSignup_Click(object sender, EventArgs e)
         {
-            username = txtUsername.Text.ToString().Trim().ToLower(); ;
+            username = txtUsername.Text.ToString().Trim().ToLower();
             fname = txtFirstname.Text.ToString().Trim();
             lname = txtLastname.Text.ToString().Trim();
             email = txtEmail.Text.ToString().Trim();
