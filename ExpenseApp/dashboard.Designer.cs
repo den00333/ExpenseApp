0ae3e46dbd5e43183f5e82448ece307213df7383
Macmac331 <bautistamacmac331@gmail.com>
2023-11-07 21:48:18 +0800

    edited some designs

--- ExpenseApp/dashboard.Designer.cs
+++ ExpenseApp/dashboard.Designer.cs
@@ -28,37 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.closeBTN = new System.Windows.Forms.PictureBox();
+            this.guna2GradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBTN)).BeginInit();
             this.SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            this.guna2GradientPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2GradientPanel1.Controls.Add(this.closeBTN);
+            this.guna2GradientPanel1.Controls.Add(this.label1);
+            this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2GradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(123)))), ((int)(((byte)(47)))));
+            this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(180)))), ((int)(((byte)(72)))));
+            this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(1479, 70);
+            this.guna2GradientPanel1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(372, 193);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Poppins Medium", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(315, 46);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "sa dashboard ito";
+            this.label1.Size = new System.Drawing.Size(253, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Expense Details";
+            // 
+            // closeBTN
+            // 
+            this.closeBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeBTN.Image = global::ExpenseApp.Properties.Resources.close;
+            this.closeBTN.Location = new System.Drawing.Point(810, 19);
+            this.closeBTN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.closeBTN.Name = "closeBTN";
+            this.closeBTN.Size = new System.Drawing.Size(57, 30);
+            this.closeBTN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.closeBTN.TabIndex = 18;
+            this.closeBTN.TabStop = false;
             // 
             // dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2GradientPanel1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "dashboard";
-            this.Size = new System.Drawing.Size(1239, 725);
+            this.Size = new System.Drawing.Size(1479, 763);
+            this.guna2GradientPanel1.ResumeLayout(false);
+            this.guna2GradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBTN)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox closeBTN;
     }
 }
