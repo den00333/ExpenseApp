58b006aec5612cdc225f71444bd5488b2f0ab769
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2023-12-13 22:25:26 +0800

    Update profile.cs

--- ExpenseApp/profile.cs
+++ ExpenseApp/profile.cs
@@ -1,5 +1,4 @@
-﻿using FireSharp.Interfaces;
-using Google.Cloud.Firestore;
+﻿using Google.Cloud.Firestore;
 using Guna.UI2.WinForms;
 using System;
 using System.Collections.Generic;
@@ -13,111 +12,211 @@ using System.Windows.Forms;
 
 namespace ExpenseApp
 {
-    public partial class profile : UserControl
+    public partial class homeForm : Form
     {
-        string username = FirebaseData.Instance.Username;
-        public profile()
+        string username;
+        connectionForm cf;
+        public homeForm()
         {
             InitializeComponent();
+
+            btnDashboard.FillColor = SystemColors.Control;
+            btnDashboard.ForeColor = Color.Black;
+            changeButtonColor(btnWallet, btnAccount, btnGroup);
+            changeFontColor(btnWallet, btnAccount, btnGroup);
+
+            dashboard dashboard = new dashboard();
+            addUserControl(dashboard);
+            username = FirebaseData.Instance.Username;
+
+            cf = new connectionForm();
+            otherFunc.checkInternet(cf, this);
+            Timer = new Timer();
+            Timer.Interval = 5000;
+            Timer.Tick += new EventHandler(timer1_Tick);
+            Timer.Start();
         }
 
-        
-        private void profile_Load(object sender, EventArgs e)
+        private async void homeForm_Load(object sender, EventArgs e)
         {
-            try
-            {
-                IFirebaseClient client = otherFunc.conn();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            displayProfile();
-            displayLogs();
+            lblFirstname.Text = "Hello, " + await otherFunc.getFirstname(username) + "!";
+            otherFunc.retrieveImage(username, pbProfile);
         }
-        public async void displayProfile()
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            otherFunc.retrieveImage(username, pbProfilepic);
-            otherFunc o = new otherFunc();
-            DocumentSnapshot snap = await o.logInFunc(username);
-            if (snap.Exists)
+            Console.WriteLine("Timer tick event occurred.");
+            if (!otherFunc.internetConn())
             {
-                FirebaseData fd = snap.ConvertTo<FirebaseData>();
-                Timestamp timestamp = fd.DateCreated;
-                lblFullname.Text = fd.FirstName + " " + fd.LastName;
-                lblEmail.Text = fd.Email;
-                lblUsername.Text = fd.Username;
-                lblBio.Text = fd.Bio;
-                if (timestamp != null)
-                {
-                    DateTime date = timestamp.ToDateTime();
-                    string dateString = date.ToString("yyyy-MM-dd");
-                    lblDate.Text = dateString;
-                }
-                homeForm home = new homeForm();
-                home.lblFirstname.Text = await otherFunc.getFirstname(username);
+                otherFunc.checkInternet(cf, this);
+            }
+            else
+            {
+                otherFunc.checkInternet(cf, this);
             }
         }
 
-        private void btnEdit_Click(object sender, EventArgs e)
+        private void homeForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            updateAcc ua = new updateAcc(this);
-            ua.ShowDialog();
-
+            Timer.Stop();
+            String username = FirebaseData.Instance.Username;
+            otherFunc.UpdateAccStatusToOffline(username);
+            otherFunc.RecordLogs(username, true, false);
+            Application.Exit();
         }
-        async Task<List<List<String>>> getDate(string username)
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            btnDashboard.FillColor = SystemColors.Control;
+            btnDashboard.ForeColor = Color.Black;
+            changeButtonColor(btnWallet, btnAccount, btnGroup);
+            changeFontColor(btnWallet, btnAccount, btnGroup);
+            if (otherFunc.internetConn())
+            {
+                dashboard dashboard = new dashboard();
+                addUserControl(dashboard);
+            }
+            else
+            {
+                NoConnectionUC nc = new NoConnectionUC();
+                addUserControl(nc);
+            }
+        }
+
+        private void btnWallet_Click(object sender, EventArgs e)
+        {
+            //soloWallet = true;
+            //groupWallet = false;
+            btnWallet.FillColor = SystemColors.Control;
+            btnWallet.ForeColor = Color.Black;
+            changeButtonColor(btnDashboard, btnAccount, btnGroup);
+            changeFontColor(btnDashboard, btnAccount, btnGroup);
+
+            if (otherFunc.internetConn())
+            {
+
+                wallet wallet = new wallet();
+                addUserControl(wallet);
+                AddExpensesForm adf = new AddExpensesForm(new wallet(), false, null, new group());
+                AddingBalanceForm abf = new AddingBalanceForm(wallet, false, null, new group());
+
+                //adf.SetWalletValues(groupWallet, soloWallet);
+                //abf.SetWalletValues(groupWallet, soloWallet);
+
+
+            }
+            else
+            {
+                NoConnectionUC nc = new NoConnectionUC();
+                addUserControl(nc);
+            }
+        }
+
+        private void btnGroup_Click(object sender, EventArgs e)
+        {
+            //groupWallet = true;
+            //soloWallet = false;
+            btnGroup.FillColor = SystemColors.Control;
+            btnGroup.ForeColor = Color.Black;
+            changeButtonColor(btnDashboard, btnAccount, btnWallet);
+            changeFontColor(btnDashboard, btnAccount, btnWallet);
+            if (otherFunc.internetConn())
+            {
+                checkGroupExists();
+                AddExpensesForm adf = new AddExpensesForm(new wallet(), true, null, new group());
+                AddingBalanceForm abf = new AddingBalanceForm(new wallet(), true, null, new group());
+                
+                //adf.SetWalletValues(groupWallet, soloWallet);
+                //abf.SetWalletValues(groupWallet, soloWallet);
+            }
+            else
+            {
+                NoConnectionUC nc = new NoConnectionUC();
+                addUserControl(nc);
+            }
+        }
+
+        private void btnAccount_Click(object sender, EventArgs e)
+        {
+            btnAccount.FillColor = SystemColors.Control;
+            btnAccount.ForeColor = Color.Black;
+            changeButtonColor(btnDashboard, btnWallet, btnGroup);
+            changeFontColor(btnDashboard, btnWallet, btnGroup);
+            if (otherFunc.internetConn())
+            {
+                profile tips = new profile();
+                addUserControl(tips);
+            }
+            else
+            {
+                NoConnectionUC nc = new NoConnectionUC();
+                addUserControl(nc);
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you really want to logout?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Timer.Stop();
+                String username = FirebaseData.Instance.Username;
+                otherFunc.UpdateAccStatusToOffline(username);
+                otherFunc.RecordLogs(username, true, false);
+                this.Hide();
+                Login login = new Login();
+                login.Show();
+            }
+        }
+        public void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            homePanel.Controls.Clear();
+            homePanel.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+        private void changeButtonColor(Guna2Button button1, Guna2Button button2, Guna2Button button3)
+        {
+            button1.FillColor = Color.FromArgb(83, 123, 47);
+            button2.FillColor = Color.FromArgb(83, 123, 47);
+            button3.FillColor = Color.FromArgb(83, 123, 47);
+        }
+        private void changeFontColor(Guna2Button button1, Guna2Button button2, Guna2Button button3)
+        {
+            button1.ForeColor = Color.White;
+            button2.ForeColor = Color.White;
+            button3.ForeColor = Color.White;
+        }
+        public async void checkGroupExists()
         {
             var db = otherFunc.FirestoreConn();
-            CollectionReference colref = db.Collection("Users").Document(username).Collection("Logs");
-            QuerySnapshot qsnap = await colref.GetSnapshotAsync();
-            List<List<String>> arrL = new List<List<String>>();
-            foreach(DocumentSnapshot docsnap in qsnap.Documents)
+            CollectionReference userCollectionRef = db.Collection("Users");
+            DocumentReference userDocRef = userCollectionRef.Document(username);
+            DocumentSnapshot userDocSnap = await userDocRef.GetSnapshotAsync();
+            if (userDocSnap.Exists && userDocSnap.ContainsField("Groups"))
             {
-                List<String> arrLogs = docsnap.GetValue<List<String>>("Login");
-                arrL.Add(arrLogs);
+                group groupControl = new group();
+                addUserControl(groupControl);
             }
-            return arrL;
-        } 
-        async void displayLogs()
+            else
+            {
+                NoGroupUC ng = new NoGroupUC();
+                addUserControl(ng);
+            }
+        }
+
+        private void cbExit_Click(object sender, EventArgs e)
         {
-             List<List<String>> logs = await getDate(username);
-            foreach (List<String> innerlist in logs)
+            DialogResult result = MessageBox.Show("Do you really want to close the application?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
             {
-                foreach (string logEntry in innerlist)
-                {
-                    string[] timeLocation = logEntry.Split('|');
-
-                    string time = timeLocation[0];
-                    string location = timeLocation[1];
-
-                    Guna2Panel pnl = new Guna2Panel();
-                    pnl.Size = new Size(465, 100);
-                    pnl.BorderRadius = 15;
-                    pnl.FillColor = Color.FromArgb(83, 123, 47);
-
-                    Label lblTime = new Label();
-                    lblTime.Font = new Font("Poppins", 9.75f, FontStyle.Regular);
-                    lblTime.Size = new Size(131, 23);
-                    lblTime.Location = new Point(38, 19);
-                    lblTime.BackColor = Color.Transparent;
-                    lblTime.ForeColor = Color.White;
-                    lblTime.Text = time;
-
-                    Label lblLocation = new Label();
-                    lblLocation.Font = new Font("Poppins", 9.75f, FontStyle.Regular);
-                    lblLocation.Size = new Size(380, 23);
-                    lblLocation.Location = new Point(38, 46);
-                    lblLocation.BackColor = Color.Transparent;
-                    lblLocation.ForeColor = Color.White;
-                    lblLocation.Text = location;
-                    
-
-                    pnl.Controls.Add(lblLocation);
-                    pnl.Controls.Add(lblTime);
-                    flpLogs.Controls.Add(pnl);
-                }
+                this.Close();
             }
-            
+        }
+
+        private void cbMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
